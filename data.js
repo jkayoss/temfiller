41771597f9d4c6e636a5d465f42880da4c648fcf
jkayoss <51717883+jkayoss@users.noreply.github.com>
2020-11-03 10:05:13 -0300

    Initial commit

---
+++ data.js
@@ -0,0 +1,46 @@
+export const lista = [
+    {name: 'Naruto', link: 'naruto'},
+    {name: 'Naruto Shippuden', link: 'shippuden'},
+    {name: 'One Piece', link: 'onepiece'},
+    {name: 'Boruto', link: 'boruto'},
+    {name: 'Bleach', link: 'bleach'},
+    {name: 'Hunter x Hunter', link: 'hunterhunter'}
+];
+
+export const shippuden = {
+    'fillers': '6-7, 11, 16, 19, 24-25, 28, 45, 57-71, 90-112, 144-151, 170-171, 176-196, 223-242, 257-260, 271, 279-281, 284-295, 303-320, 347-361, 376-377, 385-386, 389-390, 394-413, 416, 422-423, 427-450, 464-468, 480-483',
+    'canon': '1-5, 8-10, 12-15, 17-18, 20-23, 26-27, 29-44, 46-56, 72-89, 113-143, 152-169, 172-175, 197-222, 243-256, 261-270, 272-278, 282-283, 296-302, 321-346, 362-375, 378-384, 387-388, 391-393, 414-415, 417-421, 424-426, 451-463, 469-479, 484-500',
+    'src': 'https://logodownload.org/wp-content/uploads/2014/10/naruto-shippuden-logo.png',
+    'about': 'Naruto é uma série de mangá escrita e ilustrada por Masashi Kishimoto, que conta a história de Naruto Uzumaki, um jovem ninja que constantemente procura por reconhecimento e sonha em se tornar Hokage, o ninja líder de sua vila. <br> A história é dividida em duas partes, a primeira parte se passa nos anos da pré-adolescência de Naruto, e a segunda parte se passa em sua adolescência.'
+};
+
+export const onepiece = {
+    'canon': '1-49, 52-53, 62-92, 94-97, 100, 103-130, 144-195, 207-212, 217-219, 227-278, 284-290, 293-302, 304-316, 320-325, 337-381, 385-405, 408-425, 430-456, 459-491, 493-496, 500-505, 507-541, 543-574, 579-589, 591-625, 629-746, 752-774, 776-779, 783-806, 808-876',
+    'fillers': '50-51, 54-61, 93, 98-99, 101-102, 131-143, 196-206, 213-216, 220-226, 279-283, 291-292, 303, 317-319, 326-336, 382-384, 406-407, 426-429, 457-458, 492, 497-499, 506, 542, 575-578, 590, 626-628, 747-751, 775, 780-782, 807',
+    'src': 'https://www.pngkey.com/png/full/305-3059206_time-one-piece.png'
+};
+
+export const naruto = {
+    'canon': '1-25, 27-52, 54-56, 58-96, 98, 100, 107-135',
+    'fillers': '26, 53, 57, 97, 99, 101-106, 136-220',
+    'src': 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Naruto_logo.svg/1024px-Naruto_logo.svg.png',
+    'about': 'Naruto é uma série de mangá escrita e ilustrada por Masashi Kishimoto, que conta a história de Naruto Uzumaki, um jovem ninja que constantemente procura por reconhecimento e sonha em se tornar Hokage, o ninja líder de sua vila. <br> A história é dividida em duas partes, a primeira parte se passa nos anos da pré-adolescência de Naruto, e a segunda parte se passa em sua adolescência.'
+};
+
+export const boruto = {
+    'canon': '1-15, 18-32, 34-39, 51, 53-59, 61-66, 93-95',
+    'fillers': '16-17, 33, 40-50, 52, 60, 67-92, 96-97',
+    'src': 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Boruto_logo.png/640px-Boruto_logo.png'
+};
+
+export const bleach = {
+    'canon': '1-32, 34-49, 51-63, 110-127, 138-146, 150-167, 190-203, 206-212, 215-226, 267-283, 285-286, 288-297, 300-302, 306-310, 343-346, 348-350, 352-354, 356, 358-366',
+    'fillers': '33, 50, 64-109, 128-137, 147-149, 168-189, 204-205, 213-214, 227-266, 284, 287, 298-299, 303-305, 311-342, 347, 351, 355, 357',
+    'src': 'https://img.pngio.com/hd-bleach-bleach-anime-logo-transparent-transparent-png-image-bleach-logo-transparent-1281_278.png'
+};
+
+export const hunterhunter = {
+    'canon': '1-12, 14-25, 27-148',
+    'fillers': '13,26',
+    'src': 'https://upload.wikimedia.org/wikipedia/commons/1/1d/Hunter-x-hunter.png'
+};
